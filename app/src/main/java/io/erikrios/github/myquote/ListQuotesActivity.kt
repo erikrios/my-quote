6eb29395d5e054ffd66108f93b70903d70485c8d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-07 13:01:22 +0700

    Integrated view binding and set action bar title

--- app/src/main/java/io/erikrios/github/myquote/ListQuotesActivity.kt
+++ app/src/main/java/io/erikrios/github/myquote/ListQuotesActivity.kt
@@ -1,11 +1,22 @@
 package io.erikrios.github.myquote
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.myquote.databinding.ActivityListQuotesBinding
 
 class ListQuotesActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = ListQuotesActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityListQuotesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_quotes)
+        binding = ActivityListQuotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "List of Quotes"
     }
 }

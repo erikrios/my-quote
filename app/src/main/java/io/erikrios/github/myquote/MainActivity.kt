79866a0de3a201ed9b83cf59bb7a11c618080a78
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-07 12:41:02 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/myquote/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myquote/MainActivity.kt
@@ -2,10 +2,19 @@ package io.erikrios.github.myquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.erikrios.github.myquote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

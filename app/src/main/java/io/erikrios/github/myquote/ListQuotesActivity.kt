a00b40d563a6b23ea9d6d64e30ea3e09878870df
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-07 13:13:37 +0700

    Implements LoopJ to get the list of quotes

--- app/src/main/java/io/erikrios/github/myquote/ListQuotesActivity.kt
+++ app/src/main/java/io/erikrios/github/myquote/ListQuotesActivity.kt
@@ -1,8 +1,16 @@
 package io.erikrios.github.myquote
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
 import io.erikrios.github.myquote.databinding.ActivityListQuotesBinding
+import org.json.JSONArray
 
 class ListQuotesActivity : AppCompatActivity() {
 
@@ -18,5 +26,63 @@ class ListQuotesActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.title = "List of Quotes"
+
+        getListQuotes()
+    }
+
+    private fun getListQuotes() {
+        binding.progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "tps://quote-api.dicoding.dev"
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                binding.progressBar.visibility = View.INVISIBLE
+                val listQuote = ArrayList<String>()
+
+                val result = String(responseBody as ByteArray)
+                Log.d(TAG, result)
+                try {
+                    val jsonArray = JSONArray(result)
+
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        val quote = jsonObject.getString("en")
+                        val author = jsonObject.getString("author")
+                        listQuote.add("\n$quote\n - $author\n")
+                    }
+
+                    val adapter = ArrayAdapter(
+                        this@ListQuotesActivity,
+                        android.R.layout.simple_list_item_1,
+                        listQuote
+                    )
+                    binding.listQuotes.adapter = adapter
+
+                } catch (e: Exception) {
+                    Toast.makeText(this@ListQuotesActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                binding.progressBar.visibility = View.INVISIBLE
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+                Toast.makeText(this@ListQuotesActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
